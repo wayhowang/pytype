75f7f383735bb97ce8aeee94c6f0d13ba80dae22
Weihao WANG <w.weihao@qq.com>
2022-06-30 16:41:27 +0800

    indent tempfile.py use tab=2

--- pytype/tools/tempfile.py
+++ pytype/tools/tempfile.py
@@ -5,20 +5,20 @@ import os
 
 # Windows Cannot open a temp file twice without delete=False"""
 if sys.platform == 'win32':
-    class NamedTemporaryFile:
-        def __init__(self, mode='w+b', buffering=-1, encoding=None,
-                        newline=None, suffix=None, prefix=None,
-                        dir=None, delete=True, *, errors=None):
-            self._tempfile = tempfile.NamedTemporaryFile(mode=mode, buffering=buffering, encoding=encoding, 
-                newline=newline, suffix=suffix, prefix=prefix, dir=dir, delete=False, errors=errors)
-            self._delete = delete
+  class NamedTemporaryFile:
+    def __init__(self, mode='w+b', buffering=-1, encoding=None,
+            newline=None, suffix=None, prefix=None,
+            dir=None, delete=True, *, errors=None):
+      self._tempfile = tempfile.NamedTemporaryFile(mode=mode, buffering=buffering, encoding=encoding, 
+        newline=newline, suffix=suffix, prefix=prefix, dir=dir, delete=False, errors=errors)
+      self._delete = delete
 
-        def __enter__(self):
-            return self._tempfile.__enter__()
+    def __enter__(self):
+      return self._tempfile.__enter__()
 
-        def __exit__(self, *args, **kwargs):
-            self._tempfile.__exit__(*args, **kwargs)
-            if self._delete:
-                os.remove(self._tempfile.name)
+    def __exit__(self, *args, **kwargs):
+      self._tempfile.__exit__(*args, **kwargs)
+      if self._delete:
+        os.remove(self._tempfile.name)
 else:
-    NamedTemporaryFile = tempfile.NamedTemporaryFile
+  NamedTemporaryFile = tempfile.NamedTemporaryFile
